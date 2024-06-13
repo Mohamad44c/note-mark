288b9625166db67815568528f07fa9930a5063b4
Mohamad Chebli <mchebli008@hotmail.com>
2024-06-13 23:26:52 +0300

    Template code
    Template code includes config, tailwind, base css, basic layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>
+import { Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
+
+const App = () => {
+  return (
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2">Sidebar</Sidebar>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+      </RootLayout>
+    </>
+  )
 }
 
 export default App
